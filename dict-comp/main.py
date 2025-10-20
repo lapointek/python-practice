d1587333a5a18950926e0691e48f6ff82828eea9
lapointek <lapointek.dev@gmail.com>
2025-10-20 18:18:25 -0400

    chore: add dict-comp

---
+++ dict-comp/main.py
@@ -0,0 +1,9 @@
+names = ["alex", "beth", "caroline", "dave", "eleanor", "freddie"]
+import random
+
+students_scores = {student: random.randint(1, 100) for student in names}
+
+
+passed_students = {
+    student: score for (student, score) in students_scores.items() if score >= 60
+}
