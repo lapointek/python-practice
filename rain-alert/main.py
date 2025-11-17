860df16cc3e12241c1f1bb1738e8797aa912c456
lapointek <lapointek.dev@gmail.com>
2025-11-17 15:05:41 -0500

    refactor(rain-alert): update logic

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,14 +1,13 @@
 import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/weather"
 api_key = "5fe1bb085f9b36f7607cb07c7436a1b2"
 
-lat = 43.65
-lng = -79.38
+weather_params = {
+    "lat": 43.65,
+    "lon": -79.38,
+    "appid": api_key,
+}
 
-response = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lng}&appid={api_key}"
-)
-
-response_json = response.json()
-response_status = response.status_code
-print(response_status)
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.json())
