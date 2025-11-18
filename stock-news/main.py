5a13a51dc03ff1bfebbe41bd3fe08ffa84412d32
lapointek <lapointek.dev@gmail.com>
2025-11-18 17:32:36 -0500

    feat: add conditional to display up_down arrows refactor: update diff percent and formatted article

--- stock-news/main.py
+++ stock-news/main.py
@@ -30,13 +30,20 @@ print(day_before_yesterday_closing_price)
 difference = abs(
     float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
 )
+up_down = None
+
+if difference > 0:
+    up_down = "↑"
+else:
+    up_down = "↓"
+
 print(difference)
 
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
 # 2.
-if diff_percent > 1:
+if abs(diff_percent) > 1:
     news_params = {
         "apikey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -51,7 +58,16 @@ print(three_articles)
 
 
 # 3.
-[
-    f"Headline: {article['title']}. \nBrief: {article ['description']}"
-    for articles in three_articles
+formatted_articles = [
+    f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}"
+    for article in three_articles
 ]
+
+# client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+# for article in formatted_articles:
+#     message = client.messages.create(
+#         body=article,
+#         from="twilio_number",
+#         to="phone_number"
+#     )
