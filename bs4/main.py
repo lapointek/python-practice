5c90802d0ad9bcb7ba1e3758477df597c58c9bb8
lapointek <lapointek.dev@gmail.com>
2025-11-21 19:24:13 -0500

    chore: add lxml parser

--- bs4/main.py
+++ bs4/main.py
@@ -1,2 +1,11 @@
+from bs4 import BeautifulSoup
+import lxml
+
 with open("website.html", "r", encoding="utf-8") as file:
     html_content = file.read()
+
+
+soup = BeautifulSoup(html_content, "lxml")
+# print(soup.title)
+# print(soup.title.string)
+print(soup.prettify())
