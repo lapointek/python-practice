52e542755def50ccb1cdf412d61111eccde18daa
lapointek <lapointek.dev@gmail.com>
2025-11-24 15:23:08 -0500

    feat: add logic to use beautifulsoup to find classes and id

--- bs4/main.py
+++ bs4/main.py
@@ -1,11 +1,44 @@
 from bs4 import BeautifulSoup
-import lxml
 
-with open("website.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
+# import lxml
+import requests
 
+response = requests.get("https://news.ycombinator.com/news")
 
-soup = BeautifulSoup(html_content, "lxml")
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
 # print(soup.title)
-# print(soup.title.string)
-print(soup.prettify())
+
+print(soup.find_all(name="a", href=True))
+
+
+# with open("website.html", "r", encoding="utf-8") as file:
+#     html_content = file.read()
+
+
+# soup = BeautifulSoup(html_content, "lxml")
+# # print(soup.title)
+# # print(soup.title.string)
+# # print(soup.prettify())
+
+# all_anchor_tags = soup.find_all(name="a")
+# # print(all_anchor_tags)
+
+# # for tag in all_anchor_tags:
+# # print(tag.getText())
+# # print(tag.get("href"))
+
+
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+
+
+# name = soup.select_one(selector="#name")
+# print(name)
+
+# headings = soup.select(".heading")
+# print(headings)
