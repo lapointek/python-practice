a62123af64d7ac3698fc1bb7133255d951410d64
lapointek <lapointek.dev@gmail.com>
2025-11-25 16:31:11 -0500

    chore: add selenium directory

---
+++ selenium/main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
