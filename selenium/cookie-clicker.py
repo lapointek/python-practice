777732a06a608020e7c3091c801130ba256335f8
lapointek <lapointek.dev@gmail.com>
2026-03-05 19:08:56 -0500

    chore: add cookie-clicker directory

---
+++ selenium/cookie-clicker.py
@@ -0,0 +1,22 @@
+from itertools import count
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep, time
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+button = driver.find_element(By.CSS_SELECTOR, value="#bigCookie")
+
+try:
+    language = driver.find_element(by=By.ID, value="langSelect-EN")
+    language.click()
+    sleep(3)
+except:
+    print("Language slection not found")
+
+while True:
+    button.click()
