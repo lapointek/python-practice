2df92acae1a07988574e8d28173652821ff4a308
lapointek <lapointek.dev@gmail.com>
2026-03-06 14:52:34 -0500

    chore: update gym-project python script

--- selenium/gym-project.py
+++ selenium/gym-project.py
@@ -4,6 +4,8 @@ GYM_URL = "https://appbrewery.github.io/gym/"
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 
 chrome_options = webdriver.ChromeOptions()
@@ -13,4 +15,18 @@ user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://appbrewery.github.io/gym/")
+driver.get("https://appbrewery.github.io/gym/login/")
+
+wait = WebDriverWait(driver, 10)
+
+email = wait.until(EC.presence_of_element_located((By.ID, "email-input")))
+password = wait.until(EC.presence_of_element_located((By.ID, "password-input")))
+
+email.send_keys(ACCOUNT_EMAIL)
+password.send_keys(ACCOUNT_PASSWORD)
+
+button = wait.until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, ".Login_submitButton__tJFna"))
+)
+
+button.click()
